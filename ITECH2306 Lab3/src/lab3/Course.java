25bf95c53dd534783a0565a9bd87f8929cb63593
Nine-H <nine@localhost.localdomain>
2018-03-15 11:59:09 +1100

    added course class

---
+++ ITECH2306 Lab3/src/lab3/Course.java
@@ -0,0 +1,26 @@
+package System;
+
+
+/**
+ * @author 30331671
+ * @version 1.0
+ * @created 15-Mar-2018 11:54:50 AM
+ */
+public class Course {
+
+	private int Days;
+	private String Name;
+	private double Price;
+	private Person Student;
+
+	public Course(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public boolean IsRightLength(){
+		return false;
+	}
+}//end Course
